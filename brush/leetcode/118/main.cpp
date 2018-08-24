8b92476dd51660faa73b6cb24743a52ff0fb0c06
haoyifan <haoyifan@umich.edu>
2018-08-24 13:05:36 -0500

    optimize

--- brush/leetcode/118/main.cpp
+++ brush/leetcode/118/main.cpp
@@ -12,8 +12,8 @@ class Solution {
 			result.push_back(std::move(vector<int>({1, 1}))); 
 			if(numRows == 2) return result;
 
-			vector<int> curr = result.back();
 			for(int i = 2; i < numRows; ++i) {
+				const vector<int>& curr = result.back();
 				vector<int> next(curr.size() + 1);
 				next.front() = 1;
 				next.back() = 1;
@@ -21,7 +21,6 @@ class Solution {
 					next[j] = curr[j - 1] + curr[j];
 				}
 				result.push_back(std::move(next));
-				curr = result.back();
 			}
 			return result;
 		}
